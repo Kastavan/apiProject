13afcaab0c1188412fd8e780c40fcfd09e80c52a
Kastavan <kastavan4eg@gmail.com>
2023-01-28 00:28:54 +0300

    First API testing project including logger, allure, self HTTP methods

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,54 @@
+from utils.checking import Checking
+from utils.api import GoogleMapsApi
+import allure
+
+
+"""Create, update, and delete new location"""
+
+
+@allure.epic("Test create place")
+class TestCreatePlace:
+
+    @allure.description("Test create, update, and delete new location")
+    def test_create_new_place(self):
+        print("POST method")
+        result_post = GoogleMapsApi.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, 'status', 'OK')
+
+        print("GET-POST method")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types',
+                                               'website', 'language'])
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
+
+        print("PUT method")
+        result_put = GoogleMapsApi.update_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
+
+        print("GET-PUT method")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types',
+                                               'website', 'language'])
+        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
+
+        print("DELETE method")
+        result_delete = GoogleMapsApi.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
+
+        print("GET-DELETE method")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
+
+        print("Testing of create, update, and delete new location complete successfully")
